0cab80ce4151b22b32ac84ffe6a2f8e28e5adfbb
Yizhe Zhan <yizhe@illinois.edu>
2018-05-11 15:25:48 -0500

    update MOD35_download on BWs

--- preparation/MOD35_downloader.py
+++ preparation/MOD35_downloader.py
@@ -1,90 +1,16 @@
 
-# from ftplib import FTP
-# from my_module import np, os, sys
-# import urllib
-# import csv
-# import mpi4py.MPI as MPI
 
 
-# #
-# def download_date(year, day):
-
-#     # Login MODIS FTP and change to the specified directory
-#     ftp = FTP('ladsweb.nascom.nasa.gov')
-#     ftp.login('anonymous', passwd='smyiz@gmail.com')
-
-#     # data folder
-#     idate = '{}/{}'.format(iyr, str(day).zfill(3))
-#     ifolder_server = os.path.join('/allData/61/MOD35_L2', idate)
-
-#     # catch errors of bad days (e.g., June 31)
-#     try:
-#         ftp.cwd(ifolder_server)
-#     except Exception as err:
-#         print ">> cwd err: {} on {}".format(err, ifolder_server)
-#         return
-
-    
-#     # generate download list
-#     files = ftp.nlst()
-#     hdf_files = np.array([i for i in files if i.endswith('hdf')])
-
-#     if len(hdf_files) == 0:
-#         return
+"""
+LOG: 2018.05.11
+This script is used for downloading large-scale MOD35 data, which can be modified to download any other MODIS data.
+It uses urllib2 and shutil to set up the download pipline and signal to watch the download process. It is a localized version of
+the LAADS DAAC code: https://ladsweb.modaps.eosdis.nasa.gov/tools-and-services/data-download-scripts/
 
 
-#     # mkdir destination folder on BWs, continue if it is already existed
-#     ifolder_local = "/u/sciteam/smzyz/scratch/data/MODIS/MOD35/{}".format(idate)
-    
-#     try:
-#         os.mkdir(ifolder_local)
-#     except:
-#         #print ">> mkdir err: Folder already exists, continue"
-#         os.chdir(ifolder_local)
-
-
-#     # download MOD35 files to the destination folder, continue if it is already existed
-#     exist_files = os.listdir(ifolder_local)
-    
-#     for ifile in hdf_files:
-#         if ifile in exist_files:
-#             continue
-        
-#         try:
-#             with open("{}/{}".format(ifolder_local, ifile), 'wb') as f:
-#                 ftp.retrbinary('RETR {}'.format(ifile), f.write)
-#         except Exception as err:
-#             print ">> download err: {}, program terminated".format(err)
-#             return 
-
-
-
-# def download_date_v2(iyr, iday):
-#     dst_parent_folder = "/u/sciteam/smzyz/scratch/data/MODIS/MOD35"
-#     src_parent_folder = 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2'
-    
-    
-#     # 1) Download .csv file containing all available data files
-#     src_csv_file = "{}/{}/{}.csv".format(src_parent_folder, iyr, str(iday).zfill(3))
-#     dst_csv_file = "{}/{}.csv".format(dst_parent_folder, str(iday).zfill(3))
-        
-#     print src_csv_file, dst_csv_file
-#     urllib.urlretrieve(src_csv_file, dst_csv_file)
-    
-    
-#     # 2) Read csv file and generate a download file list
-#     files = [ f['name'] for f in csv.DictReader(open(dst_csv_file), skipinitialspace=True) ]
-#     src_file_list = ["{}/{}/{}/{}".format(src_parent_folder, iyr, str(iday).zfill(3), f) for f in files]
-#     dst_file_list = ["{}/{}".format(dst_parent_folder, f) for f in files]
-    
-
-#     # 3) Download
-#     for i in tqdm(range(len(src_file_list))):
-#         isrc = src_file_list[i]
-#         idst = dst_file_list[i]
-
-#         urllib.urlretrieve(isrc, idst)
-            
+One main issue remaining is that it is not clear how many download connections can be set simultantaneously. Additionally, downloaded files
+have not been checked sizes, which is important and should be implemented in the future.
+"""
 
 
 
@@ -96,69 +22,107 @@ import shutil
 import ssl
 import urllib2
 import mpi4py.MPI as MPI
+import signal
 
 
 
+# Don't know whether it is needed.
 USERAGENT = 'tis/download.py_1.0--' + sys.version.replace('\n','').replace('\r','')
 
 
+
+# Raises exception when download got stucked.
+def signal_handler(signum, frame):
+    raise Exception("Timed out!")
+
+    
+    
+# Main function of download one file.
+# Thit function actually could raise several kinds of errors, including:
+# urllib2.HTTPError
+# urllib2.URLError
+# OutOfTime error defined by signal_handler.
 def url_retrieve(src, dst):
     url = src
     headers = { 'user-agent' : USERAGENT }
     headers['Authorization'] = 'Bearer ' + "59451810-53C9-11E8-9F28-C61EAE849760"
     CTX = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     
-    try:
-        fh = urllib2.urlopen(urllib2.Request(url, headers=headers), context=CTX)
-        shutil.copyfileobj(fh, dst)
-    except urllib2.HTTPError as e:
-        print 'HTTP GET error code: %d' % e.code()
-        print 'HTTP GET error message: %s' % e.message
-    except urllib2.URLError as e:
-        print 'Failed to make request: %s' % e.reason
+    
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(120)   # 120 seconds
+
+    
+    fh = urllib2.urlopen(urllib2.Request(url, headers=headers), context=CTX)
+    shutil.copyfileobj(fh, dst)
+    #except urllib2.HTTPError as e:
+    #    print '>> HTTPError: {}'.format(url)
+    #except urllib2.URLError as e:
+    #    print '>> URLError: {}'.format(url)
+
+    
 
 
-
+# Main function of download all granules within a particular day.
 def main(iyr, iday):
     iday = str(iday).zfill(3)
     
-    dst_parent_folder = "/u/sciteam/smzyz/scratch/data/MODIS/MOD35"
+    # Set up download folder (dst_parent_folder)
+    dst_parent_folder = "/u/sciteam/smzyz/scratch/data/MODIS/MOD35/{}" .format(iyr)
+    dst_daily_folder = os.path.join(dst_parent_folder, iday)
     try:
-        os.mkdir("{}/{}/{}".format(dst_parent_folder, iyr, iday))
-        dst_parent_folder = "{}/{}/{}".format(dst_parent_folder, iyr, iday)
+        # print ">> log: {}".format(dst_daily_folder)
+        os.mkdir(dst_daily_folder)
     except OSError as err:
-        dst_parent_folder = "{}/{}/{}".format(dst_parent_folder, iyr, iday)
-        return 
-    
-    src_parent_folder = 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2'
+        pass
     
     
-    # 1) Download .csv file containing all available data files
-    src_csv_file = "{}/{}/{}.csv".format(src_parent_folder, iyr, iday)
+    # Set up server folder (src_parent_folder)
+    src_parent_folder = 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2/{}'.format(iyr)
+    src_daily_folder = os.path.join(src_parent_folder, iday)
+    
+    
+    # 1) Download daily .csv file from (src_parent_folder) to (dst_parent_folder)
+    #    e.g., src: https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2/2000/300.csv
+    #          dst: /u/sciteam/smzyz/scratch/data/MODIS/MOD35/2000/300.csv
+    src_csv_file = "{}/{}.csv".format(src_parent_folder, iday)
     dst_csv_file = "{}/{}.csv".format(dst_parent_folder, iday)
-    with open(dst_csv_file, 'w+b') as out:
-        url_retrieve(src_csv_file, out)
+    try:
+        with open(dst_csv_file, 'w+b') as out:
+            url_retrieve(src_csv_file, out)
+    except Exception as err:
+        print ">> Err: cannot download {}, move on to next day.".format(src_csv)
+        return
     
-    
-    # 2) Read csv file and generate a download file list
-    files = [ f['name'] for f in csv.DictReader(open(dst_csv_file), skipinitialspace=True) ]
-    src_file_list = ["{}/{}/{}/{}".format(src_parent_folder, iyr, iday, f) for f in files]
-    dst_file_list = ["{}/{}".format(dst_parent_folder, f) for f in files]
-    
-    
-    # 3) Download
-    for i in range(len(src_file_list)):
-        isrc = src_file_list[i]
-        idst = dst_file_list[i]
-        try:
-            with open(idst, 'w+b') as out:
-                url_retrieve(isrc, out)
-        except Exception as err:
-            #print ">> err: {}".format(err)
-            continue
-            
-         
+
+    # 2) Read csv file and generate a download file list by cross-referencing existed MOD35 files
+    files_server = [ f['name'] for f in csv.DictReader(open(dst_csv_file), skipinitialspace=True) ]
+    # sizes_server = [ f['size'] for f in csv.DictReader(open(dst_csv_file), skipinitialspace=True) ]
+    files_local = os.listdir(dst_daily_folder)
+    # sizes_local = [os.path.getsize(os.path.join(dst_daily_folder, ifile)) for ifile in files_local]
+    files_new = [ifile for ifile in files_server if ifile not in files_local]
+
+    if len(files_new) == 0:
+        return
+    else:
+        src_file_list = ["{}/{}".format(src_daily_folder, f) for f in files_new]
+        dst_file_list = ["{}/{}".format(dst_daily_folder, f) for f in files_new]
         
+    
+        # 3) Download
+        print ">> Download: {}: {} files".format(dst_daily_folder, len(files_new))
+        for i in tqdm(range(len(src_file_list))):
+            isrc = src_file_list[i]
+            idst = dst_file_list[i]
+            try:
+                with open(idst, 'w+b') as out:
+                    url_retrieve(isrc, out)
+            except Exception as err:
+                print ">> Err: cannot download {}, move on to next granule.".format(isrc)
+                continue
+           
+
+
 def times_gen(itype):
     """
     Generate times for processing, itype could be 1 or 2.
@@ -167,7 +131,7 @@ def times_gen(itype):
     """
     times = []
     if itype == 1:
-        for iyr in range(2000, 2016):
+        for iyr in range(2000, 2001):
             for iday in range(1, 367):
                 tmp = "{}{}".format(iyr, str(iday).zfill(3))
                 times.append(tmp)
@@ -179,7 +143,6 @@ def times_gen(itype):
                 times.append(tmp)
 
     return np.array(times)
-
 
 
 
@@ -199,14 +162,12 @@ if __name__ == "__main__":
     
     for idd in range(0, len(times), NUM_CORES):
         itime = times[idd+comm_rank]
-        print ">> log: PE {} process {}".format(comm_rank, itime)
-
+        
         try:
             main(int(itime[:4]), int(itime[4:]))
         except Exception as err:
-            #print ">> err: {}".format(err)
+            print ">> err: {}".format(err)
             continue
     
-    
-    
+
     
